b6c19382824d63eef4c9578227ab2b7aaf4de874
rainymz <54558606+rainymz@users.noreply.github.com>
2020-10-14 21:56:49 +0800

    Add files via upload

---
+++ courseData.js
@@ -0,0 +1,31 @@
+const courseData = [{
+    className: "Modern Web Programming",
+    subject: "COMP",
+    courseNum: "426",
+    component: "Lecture",
+    instructor: "Ketan Mayer-Patel"
+}, {
+    className: "Files And Databases",
+    subject: "COMP",
+    courseNum: "521",
+    component: "Lecture",
+    instructor: "Leonard McMillan"
+}, {
+    className: "Algorithms & Analysis",
+    subject: "COMP",
+    courseNum: "550",
+    component: "Lecture",
+    instructor: "John Majikes"
+}, {
+    className: "Algorithms & Analysis",
+    subject: "COMP",
+    courseNum: "550",
+    component: "Recitation",
+    instructor: "John Majikes"
+}, {
+    className: "Computational Photography",
+    subject: "COMP",
+    courseNum: "572",
+    component: "Lecture",
+    instructor: "Montek Singh"
+}];
